e04117beffe5be0b5457efa179832efc1f209ba6
Hanifah Atiya Budianto <hanifahatiyabudianto@gmail.com>
2021-05-24 20:52:34 +0700

    make-pdf

---
+++ make-pdf.js
@@ -0,0 +1,11 @@
+// babel version
+let printPdf = require('@rpii/wdio-html-reporter-pdf').default ;
+(async () => {
+            // need full paths
+            let htmlReportFile =  path.resolve(__dirname,'reports/html-reports/master-report.html');
+            let pdfFile = path.resolve(__dirname, 'reports/master-report.pdf');
+            //for linux you will need these options
+            let options = ['--no-sandbox','--disable-gpu','--disable-extensions'] ;
+            await printPdf(htmlReportFile, pdfFile, options, undefined) ;
+})();
+//https://www.npmjs.com/package/@rpii/wdio-html-reporter-pdf
